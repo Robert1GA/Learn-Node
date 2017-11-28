4d1acc6dd686a31efcba75da2fe2ea38cdfac6a5
Robert1GA <roberts.2015@yahoo.co.uk>
2017-11-27 17:59:54 -0800

    Completed Geolocation, Uploading, Custom Queries \nNext up is User Auth

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -1,5 +1,20 @@
 const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
+const multer = require('multer');
+const jimp = require('jimp');
+const uuid = require('uuid');
+
+const multerOptions = {
+  storage: multer.memoryStorage(),
+  fileFilter: function(req, file, next) {
+    const isPhoto = file.mimetype.startsWith('image/');
+    if(isPhoto) {
+      next(null, true);
+    } else {
+      next({ message: 'That file type is not allowed!'}, false);
+    }
+  }
+};
 
 exports.homePage = (req, res) => {
   console.log(req);
@@ -10,6 +25,25 @@ exports.homePage = (req, res) => {
 
 exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Add Store' });
+}
+
+exports.upload = multer(multerOptions).single('photo');
+
+exports.resize = async (req, res, next) => {
+  // check if new file
+  if(!req.file) {
+    next();
+    return;
+  } else {
+    const extension = req.file.mimetype.split('/')[1];
+    req.body.photo = `${uuid.v4()}.${extension}`;
+    // resize
+    const photo = await jimp.read(req.file.buffer);
+    await photo.resize(800, jimp.AUTO);
+    await photo.write(`./public/uploads/${req.body.photo}`);
+    // once photo is written, next
+    next();
+  }
 }
 
 exports.createStore = async (req, res) => {
@@ -26,7 +60,36 @@ exports.getStores = async (req, res) => {
 
 exports.editStore = async (req, res) => {
   // 1. Find the store given the ID
-  res.json(req.params);
+  const store = await Store.findOne({ _id: req.params.id });
   // 2. Confirm they are the owner of the stores
+  // TODO
   // 3. Render out the edit form
+  res.render('editStore', { title: `Edit ${store.name}`, store })
+}
+
+exports.updateStore = async (req, res) => {
+  // (re-)set location data to be a point
+  req.body.location.type = 'Point';
+  // find and update the store
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true, // return the new store instead of the old one
+    runValidators: true
+  }).exec();
+  req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href='/stores/${store.slug}'>View Store </a>`);
+  res.redirect(`/stores/${store._id}/edit`);
+};
+
+exports.getStoreBySlug = async (req, res, next) => {
+  const store = await Store.findOne({ slug: req.params.slug });
+  if(!store) return next();
+  res.render('store', {store, title: store.name })
+};
+
+exports.getStoreByTag = async (req, res) => {
+  const tag = req.params.tag;
+  const tagQuery = tag || { $exists: true };
+  const tagsPromise = Store.getTagsList();
+  const storesPromise = Store.find({ tags: tagQuery });
+  const [tags, stores] = await Promise.all([tagsPromise, storesPromise]);
+  res.render('tag', { tags, title: 'Tags', tag, stores })
 }
